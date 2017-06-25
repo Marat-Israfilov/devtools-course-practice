29e06dceb6b1f333c6e10e34f57b4c88e8495751
Marat_Israfilov <Grendablez@gmail.com>
2017-06-26 00:28:26 +0300

    tests

---
+++ modules/programmer-calculator/src/application.cpp
@@ -0,0 +1,141 @@
+// Copyright 2017 Israfilov Marat
+
+#include "include/application.h"
+#include "include/programmer_calculator.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+
+Application::Application() : message_("") {}
+
+void Application::help(const char* appname, const char* message) {
+    message_ =
+        std::string(message) +
+          "This is a hex, oct, bin number calculator application.\n\n" +
+          "Please provide arguments in the following format:\n\n"+
+
+          "  $ " + appname + " <first_number> <second_number> <type> <operation>\n\n" +    
+
+          "<operation> is one of '+', '-', '*', '/'\n\n" +
+          "<type> is one of '1'-HEX, '2'-OCT, '3'-BIN ";
+}
+
+bool Application::validateNumberOfArguments(int argc, const char** argv) {
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    } else if (argc != 5) {
+        help(argv[0], "ERROR: Should be 4 arguments.\n\n");
+        return false;
+    }
+    return true;
+}
+
+/*double parseDouble(const char* arg) {
+    char* end;
+    double value = strtod(arg, &end);
+
+    if (end[0]) {
+        throw std::string("Wrong number format!");
+    }
+
+    return value;
+}*/
+
+char parseOperation(const char* arg) {
+    char op;
+    if (strcmp(arg, "+") == 0) {
+        op = '+';
+    } else if (strcmp(arg, "-") == 0) {
+        op = '-';
+    } else if (strcmp(arg, "*") == 0) {
+        op = '*';
+    } else if (strcmp(arg, "/") == 0) {
+        op = '/';
+    } else {
+        throw std::string("Wrong operation format!");
+    }
+    return op;
+}
+
+std::string Application::operator()(int argc, const char** argv) {
+    Arguments args;
+
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    try {
+        args.first_number  = argv[1];
+        args.second_number = argv[2];
+        args.type          = char(argv[3]);        
+        args.operation     = parseOperation(argv[4]);
+    }
+    catch(std::string& str) {
+        return str;
+    }
+
+    ProgrammerCalculator converter; 
+    int tmp_num1;
+	int tmp_num2;
+    int tmp_result;    
+    std::ostringstream stream;
+	
+	switch (args.type) {
+     case '1':
+        tmp_num1 = converter.ConvertHexToDec(argv[1]);
+		tmp_num2 = converter.ConvertHexToDec(argv[2]);
+        break;
+     case '2':
+        tmp_num1 = converter.ConvertOctToDec(argv[1]);
+		tmp_num2 = converter.ConvertOctToDec(argv[2]);
+        break;
+     case '3':
+        tmp_num1 = converter.ConvertBinToDec(argv[1]);
+		tmp_num2 = converter.ConvertBinToDec(argv[2]);
+        break;
+     default:
+        return "Wrong type!";
+    }
+	
+    switch (args.operation) {
+     case '+':
+        tmp_result = tmp_num1 + tmp_num2;        
+        break;
+     case '-':
+        tmp_result = tmp_num1 - tmp_num2;
+        break;
+     case '*':
+        tmp_result = tmp_num1 * tmp_num2;
+        break;
+     case '/':
+        try {
+            tmp_result = tmp_num1 / tmp_num2;
+            break;
+        }
+        catch(std::string& str) {
+            return str;
+        }
+    }
+	
+	switch (args.type) {
+     case '1':
+        stream << converter.ConvertDecToHex(tmp_result);
+        break;
+     case '2':
+        stream << converter.ConvertDecToOct(tmp_result);
+        break;
+     case '3':
+        stream << converter.ConvertDecToBin(tmp_result);
+        break;
+     default:
+        return "Wrong type!";
+    }
+
+    message_ = stream.str();
+
+    return message_;
+}
---
+++ modules/programmer-calculator/test/test_application.cpp
@@ -0,0 +1,56 @@
+// Copyright 2017 Israfilov Marat
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+#include "include/application.h"
+
+using ::testing::internal::RE;
+using std::vector;
+using std::string;
+
+class ApplicationTest : public ::testing::Test {
+ protected:
+    void Act(vector<string> args_) {
+        vector<const char*> options;
+
+        options.push_back("appname");
+        for (size_t i = 0; i < args_.size(); ++i) {
+            options.push_back(args_[i].c_str());
+        }
+
+        const char** argv = &options.front();
+        int argc = static_cast<int>(args_.size()) + 1;
+
+        output_ = app_(argc, argv);
+    }
+
+    void Assert(std::string expected) {
+        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+    }
+
+ private:
+    Application app_;
+    string output_;
+};
+
+TEST_F(ApplicationTest, Do_Print_Help) {
+    vector<string> args = {};
+
+    Act(args);
+
+    Assert("This is a hex, oct, bin number calculator application.");
+}
+
+TEST_F(LengthConverterAppTest, Is_Checking_Number_Of_Arguments) {
+    vector<string> args = { "101", "110", "3" };
+
+    Act(args);
+
+    Assert("ERROR: Should be 4 arguments.*");
+}
